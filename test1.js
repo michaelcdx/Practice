797af6bbcd561c9d38d34cc009b6097dcbcd3d67
Michael <michaelzhngg@gmail.com>
2026-03-25 20:23:12 +0800

    Version 4

--- test1.js
+++ test1.js
@@ -1,3 +1,4 @@
 console.log("test1 line1");
 console.log("test1 line2");
 console.log("Version 3");
+console.log("Version 4");
--- test2.js
+++ test2.js
@@ -1,3 +1,4 @@
 console.log("test2 line1");
 console.log("test2 line2 version2");
 console.log("Version 3");
+console.log("Version 4");
